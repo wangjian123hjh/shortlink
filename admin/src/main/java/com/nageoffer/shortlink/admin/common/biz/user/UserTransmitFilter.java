bff056868069bd3d4d3f194352c863ae1e41f26f
wjj <493305086@qq.com>
2024-07-11 22:52:22 +0800

    用户登录拦截器返回友好提示信息

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,21 +1,27 @@
 package com.nageoffer.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.google.common.collect.Lists;
+import com.nageoffer.shortlink.admin.common.convention.Results;
+import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
+import com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
     private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/usr/login",
+            "/api/short-link/admin/v1/user/login",
             "/api/short-link/admin/v1/user/has-username"
     );
     @Override
@@ -23,23 +29,49 @@ public class UserTransmitFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URI.contains(requestURI)){
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            if (StrUtil.isBlank(username) || StrUtil.isBlank(token)){
-                filterChain.doFilter(servletRequest, servletResponse);
-                return;
-            }
-            Object userInfoStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoStr != null){
+            String method = httpServletRequest.getMethod();
+            if (!(ObjectUtil.equal(requestURI,"/api/short-link/admin/v1/user") && ObjectUtil.equal(method,"POST"))){
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (StrUtil.isBlank(username) || StrUtil.isBlank(token)){
+                    returnJson((HttpServletResponse) servletResponse, JSONUtil.toJsonStr(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoStr = null;
+                try {
+                    userInfoStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoStr == null){
+                        returnJson((HttpServletResponse) servletResponse, JSONUtil.toJsonStr(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                        return;
+                    }
+                }catch (Exception e){
+                    returnJson((HttpServletResponse) servletResponse, JSONUtil.toJsonStr(Results.failure(new ClientException(UserErrorCodeEnum.USER_TOKEN_FAIL))));
+                    return;
+                }
                 UserInfoDTO userInfoDTO = JSONUtil.toBean(userInfoStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
-            try {
-                filterChain.doFilter(servletRequest, servletResponse);
-            } finally {
-                UserContext.removeUser();
-            }
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
         }
 
+    }
+    private void returnJson(HttpServletResponse response,String json){
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+        }catch (IOException e){
+
+        }finally {
+            if (writer != null){
+                writer.close();
+            }
+        }
     }
 }
