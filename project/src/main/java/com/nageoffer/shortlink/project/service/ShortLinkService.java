6792d5a07a162ce2ff008aa044b86dfb1725ab47
wjj <493305086@qq.com>
2024-07-12 09:52:05 +0800

    开发短链接分页查询功能

--- project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/ShortLinkService.java
@@ -1,10 +1,15 @@
 package com.nageoffer.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.project.dao.entity.ShortLinkDO;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 public interface ShortLinkService extends IService<ShortLinkDO> {
     ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam);
+
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
 }
